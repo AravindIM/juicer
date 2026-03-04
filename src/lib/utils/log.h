29e7d4cce84ba0c46f14fcb4ae4ca544b77a6b15
Aravind I M <aravindim@yahoo.com>
2026-03-05 02:43:12 +0530

    feat(utils): added log messages

---
+++ src/lib/utils/log.h
@@ -0,0 +1,8 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#define ERRMSG_NO_INPUT_FILE "Error: No input file specified\n"
+#define ERRMSG_COMPRESSION "Error: compression failed\n"
+#define HELP_STRING "Usage: %s [-o|--output FILE] input.asm\n"
+
+#endif
