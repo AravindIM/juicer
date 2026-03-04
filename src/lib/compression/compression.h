43414227d15e44c649cc27e3e1a531598d49bc8d
Aravind I M <aravindim@yahoo.com>
2026-03-05 02:42:37 +0530

    feat(compression): added compression header

---
+++ src/lib/compression/compression.h
@@ -0,0 +1,10 @@
+#ifndef _COMPRESSION_H_
+#define _COMPRESSION_H_
+
+#define EEOF -1
+#define EWRITE -2
+#define COMPRESSION_SUCCESS 0
+
+#define ERRMSG_COMPRESSION_FAILED "Error: failed to compress file"
+
+#endif
